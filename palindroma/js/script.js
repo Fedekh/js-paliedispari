4f425b33e9a152f6a6697ae06d35774576839ec0
Fedekh <federicocet@gmail.com>
2023-03-16 15:05:04 +0100

    insert some logic rows to build the function

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -2,3 +2,57 @@
 
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma.
+
+
+
+
+// chiedo un input let perchè magari viene sovrascritto
+let userWord;
+
+// diventerà l'equivalente array dalla stringa input di partenza
+let userWordArray;
+
+// voglio indicare un controllo sottoforma di condizione, finche non mi inserisce una stringa, non si va avanti
+
+do {
+  userWord = prompt("Inserisci una parola:");
+    // se insisti a inserirmi un numero ti richiedo l'input corretto
+  if (!isNaN(userWord)) {
+    alert("Hai inserito un numero, inserisci una stringa valida.");
+  }
+} while (!isNaN(userWord));
+
+console.log('Hai inserito la stringa:', userWord ,typeof userWord);
+
+// Trasformo ora la stringa alfabetica di partenza in un array
+userWordArray = userWord.split ('');
+console.log('Ora la stringe viene trasformata in un array cosi composto:' , userWordArray);
+
+// let userWordArrayReverse = userWordArray.reverse;
+let userWordArrayReverse = userWordArray.reverse();
+console.log('ora il suo reverse è :' , userWordArrayReverse);
+
+if userWordArray === userWordArrayReverse {
+    console.log("la parola che hai inserito è palindroma");
+} else {
+    console.log("la parola che hai inserito non è palindroma");
+
+}
+
+
+
+
+
+
+
+
+// /**
+//  * Description Controlla se la parola inserita dall'utente è palindroma o meno
+//  * @param {string} wordToCheck
+//  * @returns {boolean} Return true se la parola inserita è palindroma
+//  */
+// function palindroma (wordToCheck) {
+//     // voglio indrodurre un flag
+//     let = isFalse;
+    
+// }
